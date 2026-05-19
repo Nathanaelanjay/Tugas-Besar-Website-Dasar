8e863b0ad13f17be100c56731a53c8a3ad918be0
Nathanael <152943708+Nathanaelanjay@users.noreply.github.com>
2026-05-19 09:58:50 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+/* 2372021 Nathanael A*/
+/*Navbar Menu*/
+var tombolMenu = $(".tombol-menu");
+var menu = $("nav .menu ul");
+
+function klikMenu() {
+    tombolMenu.click(function () {
+        menu.toggle();
+    });
+    menu.click(function () {
+        menu.toggle();
+    });
+}
+
+$(document).ready(function () {
+    var width = $(window).width();
+    if (width < 990) {
+        klikMenu();
+    }
+})
+
+$(window).resize(function () {
+    var width = $(window).width();
+    if (width > 989) {
+        menu.css("display", "block");
+    } else {
+        menu.css("display", "none");
+    }
+    klikMenu();
+});
